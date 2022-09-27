73b1b8ce9c1f7254604e0784008146bb08656e04
StephaneCash <kikonistephane@gmail.com>
2022-09-27 15:26:59 +0100

    add Proporty

--- models/productModel.js
+++ models/productModel.js
@@ -32,6 +32,9 @@ module.exports = (sequelize, DataTypes) => {
         },
         image: {
             type: DataTypes.STRING,
+        },
+        photoAdmin: {
+            type: DataTypes.STRING,
         }
     })
 
